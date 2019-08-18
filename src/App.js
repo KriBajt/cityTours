9e76fbbbd9ed4390fdae1774bff383decbe53cdc
kribajt <kribajt@o2.pl>
2019-08-18 20:33:06 +0200

    dodano responsywność kolumy za pomocą grida

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.scss';
 import Navbar from './components/Navbar/Navbar';
+import TourList from './components/TourList';
 
 function App() {
   return (
     <>
+
       <Navbar />
+      <TourList />
     </>
   );
 }
